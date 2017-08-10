93631b9faed80db74f0b34f1ce8ad908b7f9993a
jchaasis <jchaasis@gmail.com>
2017-08-10 13:43:13 -0400

    Add the fetch for the Houses data.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
   }
 
 
-//1. fetch the data to use
+//1. fetch the data to use for the characters page.
 
 fetch ("https://api.got.show/api/characters/")
 
@@ -14,34 +14,46 @@ fetch ("https://api.got.show/api/characters/")
   // })
 
   .then( function(data){
+    //list of the first 20 characters from the book "The World of Ice and Fire"
     let characters= [];
     for (let i = 0; i<data.length; i++){
         for (let b = 0; b<data[i].books.length; b++){
-          if (data[i].books[b] === "The World of Ice and Fire" && characters.length <= 20){
+          if (data[i].books[b] === "The World of Ice and Fire" && characters.length < 20){
             let character = `
                 <div>
                     <img src='${data[i].imageLink}'>
                     <h5>Name = ${data[i].name}</h5>
                 </div>
                             `;
-
               characters.push(character);
-
           }
-
-        }
+      }
     }
+  // append the characters to the body once the html page is built
     console.log(characters);
 
 
   });
 
-//fetch data for the houses
-//
-// fetch ("https://api.got.show/api/houses/")
-//
-//     .then(convertdata)
-//     .then( function(){
-//       for (let h = 0; h < data )
-//
-//     });
+// fetch data for the houses page
+
+fetch ("https://api.got.show/api/houses/")
+
+    .then(convertData)
+    .then( function(data){
+      //list of the first 20 houses
+      let houses = [];
+
+      for (let h = 0; h < data.length; h++){
+        if (houses.length < 20){
+        let house = `
+                  <div>
+                      <img src="${data[h].imageLink}">
+                      <h5>Name = ${data[h].name}</h5>
+                  </div>
+        `;
+            houses.push(house);
+        }
+      }
+      console.log(houses);
+    });
